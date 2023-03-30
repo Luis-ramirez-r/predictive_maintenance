10e1c5b90a8e2a0b057aadae04b856b41919531b
Luis Ramirez <luisr.ramir3z@gmail.com>
2023-03-30 22:07:00 +0000

    inicio clase 43

--- src/agg_telemetry.py
+++ src/agg_telemetry.py
@@ -1,4 +1,4 @@
-from config import input_file, output_file,ventana_features,ventana_falla
+from config import input_file, output_file,ventana_features,ventana_falla,output_ventanas_sin_fallas,output_ventanas_fallas
 import pandas as pd
 import numpy as np
 
@@ -17,15 +17,17 @@ def seleccionar_ventanas(df,ventana_features,ventana_falla):
 
 
 
-def seleccionar_ventanas_sin_fallas(ventana_features,ventana_falla,n):
-
+def seleccionar_ventanas_sin_fallas(df,ventana_features,ventana_falla,n):
+    """
+    
+    """
     df_ventana = pd.DataFrame( )
     for ix,id  in enumerate( range(1000,len(df)-1000,800)):
         df_temp = df.iloc[ix:ix+ventana_features].copy()
         if df_temp['failure'].max()!=0:
             continue
         df_temp['id_falla'] = ix
-        df_ventana = pd.concat([df_ventana,df_temp])
+        df_ventana = pd.concat([df_ventana,df_temp.iloc[:ventana_features-ventana_falla]])
 
 
     if len(df_ventana['id_falla'].unique()) < n:
@@ -37,9 +39,57 @@ def seleccionar_ventanas_sin_fallas(ventana_features,ventana_falla,n):
         df_ventana = df_ventana.query('id_falla in @ids')
         
         return df_ventana
-    
-    
+
 
 df = pd.read_parquet(input_file)
 df_ventana = seleccionar_ventanas(df,ventana_features,ventana_falla)
-df_ventana.to_parquet(output_file)
+df_ventana_sin_fallas = seleccionar_ventanas_sin_fallas(df,ventana_features,ventana_falla,len(df_ventana['id_falla'].unique()) )
+
+
+df_ventana.to_parquet(output_ventanas_fallas)
+df_ventana_sin_fallas.to_parquet(output_ventanas_sin_fallas)
+
+
+df_ventana_agg = df_ventana.groupby('id_falla').agg(mean_volt = ('volt', 'mean'),
+                                                mean_rot = ('rotate', 'mean'),
+                                                mean_press = ('pressure', 'mean'),
+                                                mean_vib = ('vibration', 'mean'),
+                                                max_volt = ('volt', 'max'),
+                                                max_rot = ('rotate', 'max'),
+                                                max_press = ('pressure', 'max'),
+                                                max_vib = ('vibration', 'max'),
+                                                min_volt = ('volt', 'min'),
+                                                min_rot = ('rotate', 'min'),
+                                                min_press = ('pressure', 'min'),
+                                                min_vib = ('vibration', 'min'),
+                                                std_volt = ('volt', 'std'),
+                                                std_rot = ('rotate', 'std'),
+                                                std_press = ('pressure', 'std'),
+                                                std_vib = ('vibration', 'std'),
+                                                failure_binary = ('failure_binary', 'max'),
+                                                failure = ('failure', 'max')                                             
+)  
+
+df_ventana_sin_fallas_agg = df_ventana_sin_fallas.groupby('id_falla').agg(mean_volt = ('volt', 'mean'),
+                                                mean_rot = ('rotate', 'mean'),
+                                                mean_press = ('pressure', 'mean'),
+                                                mean_vib = ('vibration', 'mean'),
+                                                max_volt = ('volt', 'max'),
+                                                max_rot = ('rotate', 'max'),
+                                                max_press = ('pressure', 'max'),
+                                                max_vib = ('vibration', 'max'),
+                                                min_volt = ('volt', 'min'),
+                                                min_rot = ('rotate', 'min'),
+                                                min_press = ('pressure', 'min'),
+                                                min_vib = ('vibration', 'min'),
+                                                std_volt = ('volt', 'std'),
+                                                std_rot = ('rotate', 'std'),
+                                                std_press = ('pressure', 'std'),
+                                                std_vib = ('vibration', 'std'),
+                                                failure_binary = ('failure_binary', 'max'),
+                                                failure = ('failure', 'max')
+                                                
+)  
+
+df_resultado = pd.concat([df_ventana_agg,df_ventana_sin_fallas_agg])
+df_resultado.to_parquet(output_file)
--- src/config.py
+++ src/config.py
@@ -1,6 +1,12 @@
 input_file = '../data/preprocessed_data/training_data/df_failures_telemetry.parquet'
-output_file = 'res.parquet'
+output_file = '../data/processed/training_data/df_failures_telemetry_agg.parquet'
+output_ventanas_fallas = '../data/processed/training_data/df_ventanas_fallas.parquet'
+output_ventanas_sin_fallas = '../data/processed/training_data/df_ventanas_sin_fallas.parquet'
 
-ventana_features = 35
-ventana_falla = 10
+
+
+
+ventana_features = 100
+ventana_falla = 50
+
 
